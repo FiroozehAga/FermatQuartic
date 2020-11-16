604b1c4be2cc043e3ff834ca79113e00c4feba8a
Firoozeh Aga <firoozehaga@gmail.com>
2020-11-16 22:58:19 +0100

    commented out println from the initial matrices Li, added a few more terms. Requires proper julia syntaxing

--- intersectionMatrix.jl
+++ intersectionMatrix.jl
@@ -16,20 +16,20 @@ for j in [1, 3, 5, 7]
         # n = ncols(M)
         # L1[j, k] = V(x0 + w^j * x1 , x2 + w^k * x3);
         L1 = matrix(K, [1 w^j 0 0; 0 0 1 w^k]);
-        println(L1);
+        #println(L1);
         #L2[j, k] = V(x0 + w^j * x2, x1 + w^k * x3);
         L2 = matrix(K, [1 0 w^j 0; 0 1 0 w^k]);
-        println(L2);
+        #println(L2);
         #L3[j, k] = V(x0 + w^j * x3, x1 + w^k * x2);
         L3 = matrix(K, [1 0 0 w^j; 0 1 w^k 0]);
-        println(L3);
+        #println(L3);
     end
 end
 # Write a multi-dimensional array A collecting all these matrices as entries
-A= 
+A= [L1, L2, L3];
 # Generate matrices of the form (Lm | Ln)
 # Q = vcat(M, N)
-n = length(A);
+n = length(A); #size of the array A
 for i in 1:n
     for j in 1:n
         M = vcat(A[i], A[j]);
@@ -41,17 +41,17 @@ end
 # if the rank of the matrix is 3 then save 1 as the (m,n)-th entry of a matrix M
 # if the rank of the matrix is 4 then save 0 as the (m,n)-th entry of a matrix M
 
-m = length (M);
+m = length (M); # size of matrix array M
 for i in 1:m
     r = rank(M[i]);
     if r == 2
-        m[i,j] = -2;
+        IM[i,j] = -2;
     elseif r == 3
-        m[i,j] = 1;
+        IM[i,j] = 1;
     else 
-        m[i,j] = 0;
+        IM[i,j] = 0;
     end
 end
-# Compute the rank of the 48x48 matrix m
-# rank (m)
-println(rank(m));
+# Compute the rank of the 48x48 matrix IM
+# rank (IM)
+println(rank(IM));

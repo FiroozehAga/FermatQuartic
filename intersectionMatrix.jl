21eeb29436d152fc4fb970ff5c98ce8c0170df66
Firoozeh Aga <firoozehaga@gmail.com>
2020-11-16 22:09:03 +0100

    added println to each line type Li

--- intersectionMatrix.jl
+++ intersectionMatrix.jl
@@ -15,11 +15,14 @@ for j in [1, 3, 5, 7]
         # m = nrows(M)
         # n = ncols(M)
         # L1[j, k] = V(x0 + w^j * x1 , x2 + w^k * x3);
-        L1[j, k] = matrix(K, [1 w^j 0 0; 0 0 1 w^k]);
+        L1 = matrix(K, [1 w^j 0 0; 0 0 1 w^k]);
+        println(L1);
         #L2[j, k] = V(x0 + w^j * x2, x1 + w^k * x3);
-        L2[j, k] = matrix(K, [1 0 w^j 0; 0 1 0 w^k];
+        L2 = matrix(K, [1 0 w^j 0; 0 1 0 w^k];
+        println(L2);
         #L3[j, k] = V(x0 + w^j * x3, x1 + w^k * x2);
-        L3[j, k] = matrix(K, [1 0 0 w^j; 0 1 w^k 0];
+        L3 = matrix(K, [1 0 0 w^j; 0 1 w^k 0];
+        println(L3);
     end
 end
 

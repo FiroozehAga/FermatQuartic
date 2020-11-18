03b0460b2a0c1f1bd541b78d5600f323a6457cc2
Firoozeh Aga <firoozehaga@gmail.com>
2020-11-18 20:05:13 +0100

    A function list() that generates all the 48 lines on the Fermat quartic as an Array of matrices whose rows span each line

--- intersectionMatrix.jl
+++ intersectionMatrix.jl
@@ -7,24 +7,26 @@ using Hecke
 K, w= cyclotomic_field(8::Int);
 Kx, (x0, x1, x2, x3) = PolynomialRing(K, ["x0", "x1", "x2", "x3"]);
 #function rk()
-    for j in [1, 3, 5, 7]
-        for k in [1, 3, 5, 7]
-            # Li are matrices with specific entries from the coefficient of the defining polynomial equations
-            L1 = matrix(K, [1 w^j 0 0; 0 0 1 w^k]); # L1[j, k] = V(x0 + w^j * x1 , x2 + w^k * x3);
-            #println(L1);
-            L2 = matrix(K, [1 0 w^j 0; 0 1 0 w^k]); #L2[j, k] = V(x0 + w^j * x2, x1 + w^k * x3);
-            #println(L2);
-            L3 = matrix(K, [1 0 0 w^j; 0 1 w^k 0]); #L3[j, k] = V(x0 + w^j * x3, x1 + w^k * x2);
-            #println(L3);
-            A= [L1, L2, L3]; 
-            println(A);
+    function list(w)
+        A = [] #empty array
+        for j in [1, 3, 5, 7]
+            for k in [1, 3, 5, 7]
+                # Li are matrices with specific entries from the coefficient of the defining polynomial equations
+                L1 = matrix(K, [1 w^j 0 0; 0 0 1 w^k]); # L1[j, k] = V(x0 + w^j * x1 , x2 + w^k * x3);
+                L2 = matrix(K, [1 0 w^j 0; 0 1 0 w^k]); #L2[j, k] = V(x0 + w^j * x2, x1 + w^k * x3);
+                L3 = matrix(K, [1 0 0 w^j; 0 1 w^k 0]); #L3[j, k] = V(x0 + w^j * x3, x1 + w^k * x2);
+                A = push!(A, L1, L2, L3);
+            end
         end
+        return A;
     end
+    #n = size(A); #size of the array A
+    #n = +n;    
     # Write a multi-dimensional array A collecting all these matrices as entries
     #A= [L1, L2, L3];
     # Generate matrices of the form (Lm | Ln)
     # Q = vcat(M, N)
-    n = length(A); #size of the array A
+    
     for i in 1:n
         for j in 1:n
             M = vcat(A[i], A[j]);
